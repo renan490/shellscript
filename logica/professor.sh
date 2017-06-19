79e7636d86d4d048abf41d6a16ba50f57c99e366
renan490 <renan490@gmail.com>
2017-06-19 18:52:49 -0300

    Calcula a soma, multiplicação e subtração.

---
+++ logica/professor.sh
@@ -0,0 +1,6 @@
+# Faz o calculo da soma, multiplicacao e subtracao envolvendo dois numeros
+# Esse código também utiliza argumentos ($1 e $2)
+# ./professor.sh 4 5
+echo "SOMA = $(($1+$2))
+MULTIPLICACAO = $(($1*$2))
+SUBTRACAO = $(($1-$2))"
